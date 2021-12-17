76d1bccde27f6f69d68bbde3c3d6c8e4b1d2b2da
AnDISaY <sainirgg@inbox.ru>
2021-12-17 21:30:03 +0600

    Add docs

--- shop/urls.py
+++ shop/urls.py
@@ -17,10 +17,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Python 15 shop',
+        default_version='v1',
+        description='Internet shop',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/docs/', schema_view.with_ui('swagger')),
     path('api/v1/', include('product.urls')),
     path('api/v1/', include('account.urls')),
     path('api/v1/', include('order.urls')),
